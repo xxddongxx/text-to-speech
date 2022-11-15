f5f40c219ad8202be432b23731bd5812d7e1b916
xxddongxx <hyeon2187@gmail.com>
2022-11-15 23:20:28 +0900

    FEAT: 유저 admin 추가

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,56 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from users.models import User
+
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = [
+        "id",
+        "username",
+        "date_joined",
+        "is_active",
+    ]
+    list_display_links = [
+        "id",
+        "username",
+    ]
+    fieldsets = [
+        (
+            "Profile",
+            {
+                "fields": (
+                    "username",
+                    "password",
+                    "name",
+                    "phone",
+                    "email",
+                ),
+                "classes": ("wide",),
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+                "classes": ("collapse",),
+            },
+        ),
+        (
+            "Important Dates",
+            {
+                "fields": ("last_login", "date_joined"),
+                "classes": ("collapse",),
+            },
+        ),
+    ]
+
+
+admin.site.register(User, CustomUserAdmin)
