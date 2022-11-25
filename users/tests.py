420f35eadf36e792ad110710cc27bc953ee1e607
xxddongxx <hyeon2187@gmail.com>
2022-11-25 17:45:30 +0900

    TEST: 회원가입 실패 테스트

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+
+class TestRegister(APITestCase):
+    REGISTER_URL = "/api/v1/users/register/"
+
+    def test_register_fail(self):
+        """
+        회원가입 실페 테스트
+        """
+        response = self.client.post(self.REGISTER_URL)
+        response_data = response.json()
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIsInstance(response_data, dict)
+        self.assertIn("message", response_data)
+
+        response = self.client.post(self.REGISTER_URL, {"password": "test"})
+        response_data = response.json()
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIsInstance(response_data, dict)
+        self.assertIn("message", response_data)
