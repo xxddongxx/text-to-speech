eaa4ef4ae41665402b709940de577e72f0751b25
xxddongxx <hyeon2187@gmail.com>
2022-11-25 17:32:36 +0900

    FIX: 회원가입 error Response message 수정

--- users/views.py
+++ users/views.py
@@ -18,7 +18,9 @@ class UsersRegister(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            {"message": "Fail Register"}, status=status.HTTP_400_BAD_REQUEST
+        )
 
 
 class Login(APIView):
